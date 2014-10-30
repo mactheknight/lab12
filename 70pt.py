247f2cb43a06af09471f7ea2bd24a848d5951448
mactheknight <s650551@student.roundrockisd.org>
2014-10-30 15:04:36 -0500

    70 Pt. Finished

--- 70pt.py
+++ 70pt.py
@@ -27,7 +27,12 @@ class MyApp:
 		self.button3 = Button(self.myContainer1)
 		self.button3.configure(text="Test?", background="cyan")  
 		self.button3.grid(row=0,column=2)	  ### (3)
+		
 			
+		self.button4 = Button(self.myContainer1)
+		self.button4.configure(text="Goodbye")
+		self.button4.grid(row=0,column=3)        ### (4)
+		
 	
 		
 root = Tk()
