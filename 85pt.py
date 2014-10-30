d4ee763a7b1735fc21851ace2644d34e326be10c
mactheknight <s650551@student.roundrockisd.org>
2014-10-30 15:21:06 -0500

    85 Pt. Finished

--- 85pt.py
+++ 85pt.py
@@ -24,12 +24,23 @@ class MyApp:
 		self.button1.bind("<Button-1>", self.button1Click) ### (1)
 		
 		
+		self.button2 = Button(self.myContainer1)
+		self.button2.configure(text="Cancel",background= "red")
+		self.button2.grid(row=0, column=1)
+		self.button2.bind("<Button-1>", self.button2Click)
+	
 	def button1Click(self, event):    ### (3)
 		if self.button1["background"] == "green": ### (4)
 			self.button1["background"] = "yellow"
 		else:
 			self.button1["background"] = "green"
-	
+
+	def button2Click(self, event):
+	        if self.button2["background"] == "red":
+	                self.button2["background"] = "blue"
+	        else:
+	                self.button2["background"] = "red"		
+			
 		
 root = Tk()
 myapp = MyApp(root)
